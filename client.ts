82d60525c982db4fda94c5d69951b3f8dce73ec6
wengx00 <wengx86@163.com>
2024-05-17 15:13:47 +0800

    fix: 代码优化

--- client.ts
+++ client.ts
@@ -1,3 +1,8 @@
+/*
+ * @Author: wengx00 wengx86@163.com
+ * @Date: 2024-05-17 13:32:06
+ * Copyright (c) 2024 by wengx00, All Rights Reserved.
+ */
 import chalk from "chalk";
 import { existsSync, mkdirSync, readFileSync } from "fs";
 import SFTP from "ssh2-sftp-client";
@@ -5,12 +10,12 @@ import WebSocket from "ws";
 
 // 开发机的信息
 const devCloudInfo = {
-  host: "devcloud.example.com",
-  username: "root",
-  port: 36000, // SSH 端口
-  wsPort: 8001, // WebSocket 端口，一般不需要改变
-  remoteRootPath: "Don't modify this property", // 不要改变它
-  privateKey: readFileSync('C:\\Users\\username\\.ssh\\privatekey_file').toString()
+	host: "192.168.1.2",
+	username: "root",
+	port: 36000, // SSH 端口
+	wsPort: 8001, // WebSocket 端口，一般不需要改变
+	remoteRootPath: "Don't modify this property", // 不要改变它
+	privateKey: readFileSync("/path/to/privateKeyFile").toString(),
 };
 
 let handShake = true;
@@ -18,49 +23,68 @@ let handShake = true;
 const rootPath = process.argv[2];
 
 if (!rootPath) {
-  console.log(chalk.red("[ERROR]"), "No root path specified");
-  process.exit(1);
+	console.log(chalk.red("[ERROR]"), "No root path specified");
+	process.exit(1);
 }
 
 const sftp = new SFTP();
 sftp
-.connect({
-  host: devCloudInfo.host,
-  port: devCloudInfo.port,
-  username: devCloudInfo.username,
-  privateKey: devCloudInfo.privateKey,
-})
-.then(() => {
-  console.log(chalk.grey("[SFTP]"), "Connected to server");
-  const ws = new WebSocket(`ws://${devCloudInfo.host}:${devCloudInfo.wsPort}`);
-    ws.on("message", (data) => {
-      const path = data.toString("utf8");
-      if (handShake) {
-        handShake = false;
-        devCloudInfo.remoteRootPath = path;
-        return
-      }
-      console.log(chalk.grey("[WS]"), "Received changed file", chalk.yellow(path));
-      const splittedPath = path.split("/").filter(item => item.length);
-      let currentPath = rootPath;
-      splittedPath.forEach((pathPart, index) => {
-        currentPath += "/" + pathPart;
-        if (index === splittedPath.length - 1) return;
-        if (!existsSync(currentPath)) {
-          mkdirSync(currentPath);
-        }
-      });
-      // 传输文件
-      sftp.fastGet(`${devCloudInfo.remoteRootPath}/${path}`, currentPath)
-        .then(() => {
-          console.log(chalk.grey("[SFTP]"), "Downloaded file", chalk.yellow(path));
-        })
-        .catch(err => {
-          console.log(chalk.red("[SFTP]"), "Download file", chalk.yellow(path), "failed", err)
-        })
-    });
-  })
-  .catch(err => {
-    console.log(chalk.red("[SFTP]"), "Failed to connect to server", err);
-    process.exit(1);
-  });
+	.connect({
+		host: devCloudInfo.host,
+		port: devCloudInfo.port,
+		username: devCloudInfo.username,
+		privateKey: devCloudInfo.privateKey,
+	})
+	.then(() => {
+		console.log(chalk.grey("[SFTP]"), "Connected to server");
+		handShake = true;
+		const ws = new WebSocket(
+			`ws://${devCloudInfo.host}:${devCloudInfo.wsPort}`
+		);
+		ws.on("message", (data) => {
+			const path = data.toString("utf8");
+			if (handShake) {
+				handShake = false;
+				devCloudInfo.remoteRootPath = path;
+				console.log("[INFO]", "Remote root path is", chalk.blue(path));
+				return;
+			}
+			console.log(
+				chalk.grey("[WS]"),
+				"Received changed file",
+				chalk.yellow(path)
+			);
+			const splittedPath = path.split("/").filter((item) => item.length);
+			let currentPath = rootPath;
+			splittedPath.forEach((pathPart, index) => {
+				currentPath += "/" + pathPart;
+				if (index === splittedPath.length - 1) return;
+				if (!existsSync(currentPath)) {
+					mkdirSync(currentPath);
+				}
+			});
+			// 传输文件
+			sftp
+				.fastGet(`${devCloudInfo.remoteRootPath}/${path}`, currentPath)
+				.then(() => {
+					console.log(
+						chalk.grey("[SFTP]"),
+						"Downloaded file",
+						chalk.yellow(path)
+					);
+				})
+				.catch((err) => {
+					console.log(
+						chalk.red("[SFTP]"),
+						"Download file",
+						chalk.yellow(path),
+						"failed",
+						err
+					);
+				});
+		});
+	})
+	.catch((err) => {
+		console.log(chalk.red("[SFTP]"), "Failed to connect to server", err);
+		process.exit(1);
+	});
--- server.ts
+++ server.ts
@@ -1,71 +1,76 @@
-import chokidar from "chokidar";
+/*
+ * @Author: wengx00 wengx86@163.com
+ * @Date: 2024-05-17 13:32:06
+ * Copyright (c) 2024 by wengx00, All Rights Reserved.
+ */
 import chalk from "chalk";
-import WebSocket from "ws";
+import chokidar from "chokidar";
 import { resolve } from "path";
+import WebSocket from "ws";
 
 const rootPath = process.argv[2];
 
 const history = new Set<string>();
 
 if (!rootPath) {
-  console.log(chalk.red("Please provide a root path to watch"));
-  process.exit(1);
+	console.log(chalk.red("Please provide a root path to watch"));
+	process.exit(1);
 }
 
 const wss = new WebSocket.Server({ port: 8001 }, () => {
-  console.log(
-    chalk.green("WebSocket Server is running on port"),
-    chalk.blue(wss.options.port)
-  );
+	console.log(
+		chalk.green("WebSocket Server is running on port"),
+		chalk.blue(wss.options.port)
+	);
 });
 
 const wsPool = new Set<WebSocket>();
 
 wss.on("connection", (ws) => {
-  wsPool.add(ws);
-  // 发送当前监听的 RootPath
-  ws.send(resolve(rootPath));
-  // 同步历史改动
-  history.forEach((path) => {
-    ws.send(path);
-  });
+	wsPool.add(ws);
+	// 发送当前监听的 RootPath
+	ws.send(resolve(rootPath));
+	// 同步历史改动
+	history.forEach((path) => {
+		ws.send(path);
+	});
 
-  ws.on("close", () => {
-    wsPool.delete(ws);
-  });
+	ws.on("close", () => {
+		wsPool.delete(ws);
+	});
 });
 
 const onChange = (rawPath: string) => {
-  const path = rawPath.slice(rootPath.length).replace(/\\/g, "/");
-  console.log(
-    chalk.grey("[Watcher]"),
-    chalk.grey("Change detected in"),
-    chalk.yellow(path)
-  );
-  history.add(path);
-  wsPool.forEach((ws) => {
-    ws.send(path);
-  });
+	const path = rawPath.slice(rootPath.length).replace(/\\/g, "/");
+	console.log(
+		chalk.grey("[Watcher]"),
+		chalk.grey("Change detected in"),
+		chalk.yellow(path)
+	);
+	history.add(path);
+	wsPool.forEach((ws) => {
+		ws.send(path);
+	});
 };
 
 chokidar
-  .watch(rootPath, {
-    persistent: true,
-    ignoreInitial: false,
-    usePolling: false,
-    depth: 99,
-    awaitWriteFinish: {
-      stabilityThreshold: 1000,
-      pollInterval: 100,
-    },
-    atomic: true,
-  })
-  .on("ready", () => {
-    console.log(
-      chalk.grey("[Watcher]"),
-      "Watching for changes in",
-      chalk.green(resolve(rootPath))
-    );
-  })
-  .on("change", onChange)
-  .on('add', onChange);
+	.watch(rootPath, {
+		persistent: true,
+		ignoreInitial: false,
+		usePolling: false,
+		depth: 99,
+		awaitWriteFinish: {
+			stabilityThreshold: 1000,
+			pollInterval: 100,
+		},
+		atomic: true,
+	})
+	.on("ready", () => {
+		console.log(
+			chalk.grey("[Watcher]"),
+			"Watching for changes in",
+			chalk.green(resolve(rootPath))
+		);
+	})
+	.on("change", onChange)
+	.on("add", onChange);
